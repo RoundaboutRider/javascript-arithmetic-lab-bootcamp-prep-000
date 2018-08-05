f9c91e3943bb25fa7b1afcc6c6d0a4697d160ec5
Roundabout Rider <ntm.email@icloud.com>
2018-08-05 10:05:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function add(a , b) {
 function subtract(a , b) {
   return parseInt(a , 10) - parseInt(b , 10)
 }
+
+function multiply(a , b) {
+  return parseInt(a , 10) * parseInt(b , 10)
+}
+
+function divide(a , b) {
+  return parseInt(a , 10) / parseInt(b , 10)
+}
